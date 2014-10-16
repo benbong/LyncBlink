a0c7470cc628895d1c962b90d15b55f2d626adb5
Benoit Faivre <benoit.faivre@gmail.com>
2014-10-16 21:37:24 +1100

    Initial load
    Program is not running, but starting to get the files ready.

---
+++ LyncBlink/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace LyncBlink
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.blink1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lyncBlinkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configurationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.blink1ToolStripMenuItem,
+            this.lyncBlinkToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(247, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // blink1ToolStripMenuItem
+            // 
+            this.blink1ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.configurationToolStripMenuItem});
+            this.blink1ToolStripMenuItem.Name = "blink1ToolStripMenuItem";
+            this.blink1ToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.blink1ToolStripMenuItem.Text = "blink(1)";
+            // 
+            // lyncBlinkToolStripMenuItem
+            // 
+            this.lyncBlinkToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.lyncBlinkToolStripMenuItem.Name = "lyncBlinkToolStripMenuItem";
+            this.lyncBlinkToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.lyncBlinkToolStripMenuItem.Text = "LyncBlink";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // configurationToolStripMenuItem
+            // 
+            this.configurationToolStripMenuItem.Name = "configurationToolStripMenuItem";
+            this.configurationToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.configurationToolStripMenuItem.Text = "Configuration";
+            this.configurationToolStripMenuItem.Click += new System.EventHandler(this.configurationToolStripMenuItem_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AccessibleDescription = "availabilityComboBox";
+            this.comboBox1.AccessibleName = "availabilityComboBox";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(61, 66);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 148);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem blink1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configurationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lyncBlinkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ LyncBlink/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using Microsoft.Lync.Model;
+
+
+namespace LyncBlink
+{
+    public partial class Form1 : Form
+    {
+        #region Fields
+        // Current dispatcher reference for changes in the user interface.
+        private Dispatcher dispatcher;
+        private LyncClient lyncClient;
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Save the current dispatcher to use it for changes in the user interface.
+            dispatcher = Dispatcher.CurrentDispatcher;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Add the availability values to the ComboBox
+            comboBox1.Items.Add(ContactAvailability.Free);
+            comboBox1.Items.Add(ContactAvailability.Busy);
+            comboBox1.Items.Add(ContactAvailability.DoNotDisturb);
+            comboBox1.Items.Add(ContactAvailability.Away);
+
+            //Listen for events of changes in the state of the client
+            try
+            {
+                lyncClient = LyncClient.GetClient();
+            }
+            catch (ClientNotFoundException clientNotFoundException)
+            {
+                Console.WriteLine(clientNotFoundException);
+                return;
+            }
+            catch (NotStartedByUserException notStartedByUserException)
+            {
+                Console.Out.WriteLine(notStartedByUserException);
+                return;
+            }
+            catch (LyncClientException lyncClientException)
+            {
+                Console.Out.WriteLine(lyncClientException);
+                return;
+            }
+            catch (SystemException systemException)
+            {
+                if (IsLyncException(systemException))
+                {
+                    // Log the exception thrown by the Lync Model API.
+                    Console.WriteLine("Error: " + systemException);
+                    return;
+                }
+                else
+                {
+                    // Rethrow the SystemException which did not come from the Lync Model API.
+                    throw;
+                }
+            }
+
+            lyncClient.StateChanged +=
+                new EventHandler<ClientStateChangedEventArgs>(Client_StateChanged);
+
+        }
+
+        /// <summary>
+        /// Handler for the StateChanged event of the contact. Used to update the user interface with the new client state.
+        /// </summary>
+        private void Client_StateChanged(object sender, ClientStateChangedEventArgs e)
+        {
+            //Use the current dispatcher to update the user interface with the new client state.
+
+        }
+
+        private bool IsLyncException(SystemException ex)
+        {
+            return
+                ex is NotImplementedException ||
+                ex is ArgumentException ||
+                ex is NullReferenceException ||
+                ex is NotSupportedException ||
+                ex is ArgumentOutOfRangeException ||
+                ex is IndexOutOfRangeException ||
+                ex is InvalidOperationException ||
+                ex is TypeLoadException ||
+                ex is TypeInitializationException ||
+                ex is InvalidCastException;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // N/A
+        }
+
+        private void configurationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // TODO
+            // indicate which blink(1) you have 
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // TODO
+            // display the Licence file and some information
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UpdateUserInterface(ClientState currentState)
+        {
+
+            if (currentState == ClientState.SignedIn)
+            {
+                //Listen for events of changes of the contact's information
+                lyncClient.Self.Contact.ContactInformationChanged +=
+                    new EventHandler<ContactInformationChangedEventArgs>(SelfContact_ContactInformationChanged);
+
+                //Get the contact's information from Lync and update with it the corresponding elements of the user interface.
+                SetAvailability();
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("Please sign in!", "Lync not running", MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void SetAvailability()
+        {
+            //Get the current availability value from Lync
+            ContactAvailability currentAvailability = 0;
+            try
+            {
+                currentAvailability = (ContactAvailability)
+                                                          lyncClient.Self.Contact.GetContactInformation(ContactInformationType.Availability);
+            }
+            catch (LyncClientException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (SystemException systemException)
+            {
+                if (IsLyncException(systemException))
+                {
+                    // Log the exception thrown by the Lync Model API.
+                    Console.WriteLine("Error: " + systemException);
+                }
+                else
+                {
+                    // Rethrow the SystemException which did not come from the Lync Model API.
+                    throw;
+                }
+            }
+
+
+            if (currentAvailability != 0)
+            {
+                //Update the availability ComboBox with the contact's current availability.
+                ComboBox1.SelectedValue = currentAvailability;
+
+                //Choose a color to match the contact's current availability and update the border area containing the contact's photo
+                Brush availabilityColor;
+                switch (currentAvailability)
+                {
+                    case ContactAvailability.Away:
+                        availabilityColor = Brushes.Yellow;
+                        break;
+                    case ContactAvailability.Busy:
+                        availabilityColor = Brushes.Red;
+                        break;
+                    case ContactAvailability.BusyIdle:
+                        availabilityColor = Brushes.Red;
+                        break;
+                    case ContactAvailability.DoNotDisturb:
+                        availabilityColor = Brushes.DarkRed;
+                        break;
+                    case ContactAvailability.Free:
+                        availabilityColor = Brushes.LimeGreen;
+                        break;
+                    case ContactAvailability.FreeIdle:
+                        availabilityColor = Brushes.LimeGreen;
+                        break;
+                    case ContactAvailability.Offline:
+                        availabilityColor = Brushes.LightSlateGray;
+                        break;
+                    case ContactAvailability.TemporarilyAway:
+                        availabilityColor = Brushes.Yellow;
+                        break;
+                    default:
+                        availabilityColor = Brushes.LightSlateGray;
+                        break;
+                }
+                availabilityBorder.Background = availabilityColor;
+            }
+        }
+
+    }
+}
